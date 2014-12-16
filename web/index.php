fbb5090e5e70521a0c8ed7a662a4b52e89f05831
Andreas Hucks <andreas.hucks@duochrome.net>
2014-12-16 09:59:11 +0100

    added missing annotation loader

--- web/index.php
+++ web/index.php
@@ -1,8 +1,11 @@
 <?php
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Symfony\Component\HttpFoundation\Request;
 
-require_once __DIR__.'/../vendor/autoload.php';
+$loader = require_once __DIR__.'/../vendor/autoload.php';
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
 require_once __DIR__.'/../app/Kernel.php';
 
 $kernel = new Kernel();
